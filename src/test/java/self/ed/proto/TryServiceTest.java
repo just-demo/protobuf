5bc60516ea8450cf4735c3d625c92c9c35f2e43a
Anatolii <anatolii.kryvda@gmail.com>
2019-05-21 00:35:49 +0300

    More service examples

--- src/test/java/self/ed/proto/TryServiceTest.java
+++ src/test/java/self/ed/proto/TryServiceTest.java
@@ -1,15 +1,34 @@
 package self.ed.proto;
 
-import com.google.protobuf.RpcCallback;
-import com.google.protobuf.RpcController;
+import com.google.protobuf.*;
 import org.junit.Test;
 
 public class TryServiceTest {
     @Test
-    public void testInterface() {
-        DemoService service = new DemoService() {
+    public void testCustom() {
+        CustomService service = new CustomService() {
             @Override
             public void demo(RpcController controller, ServiceRequest request, RpcCallback<ServiceResponse> done) {
+                // ...
+            }
+        };
+    }
+
+    @Test
+    public void testWrapper() {
+        WrapperService service = new WrapperService() {
+            @Override
+            public void demo(RpcController controller, StringValue request, RpcCallback<BoolValue> done) {
+                // ...
+            }
+        };
+    }
+
+    @Test
+    public void testEmpty() {
+        EmptyService service = new EmptyService() {
+            @Override
+            public void demo(RpcController controller, Empty request, RpcCallback<Empty> done) {
                 // ...
             }
         };
