ed36fdd74fcf9e127350165040865ef8bfe30e26
Anatolii Kryvda <anatolii.kryvda@automationanywhere.com>
2019-05-28 16:31:59 +0300

    More examples for demo

---
+++ src/main/java/demo/Int32vs64Main.java
@@ -0,0 +1,23 @@
+package demo;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
+import com.hubspot.jackson.datatype.protobuf.ProtobufModule;
+import self.ed.proto.intvslong.Demo;
+
+public class Int32vs64Main {
+    public static void main(String[] args) throws InvalidProtocolBufferException, JsonProcessingException {
+        Demo demo = Demo.newBuilder()
+                .setA(123)
+                .setB(123)
+                .build();
+        System.out.println(JsonFormat.printer().omittingInsignificantWhitespace().print(demo)); // {"a":123,"b":"123"}
+        System.out.println(new ObjectMapper().registerModule(new ProtobufModule()).writeValueAsString(demo));
+
+        Demo.Builder builder = Demo.newBuilder();
+//        JsonFormat.parser().merge("{\"a\":123,\"b\":\"Infinity\"}", builder); // an error is thrown
+        System.out.println(builder.build());
+    }
+}
--- src/main/java/demo/UserMapperMain.java
+++ src/main/java/demo/UserMapperMain.java
@@ -1,10 +1,21 @@
 package demo;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.util.JsonFormat;
+import com.hubspot.jackson.datatype.protobuf.ProtobufModule;
+import com.just.demo.DemoCamel;
+import com.just.demo.User;
+import com.just.demo.Users;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
 
 public class UserMapperMain {
-    public static void main(String[] args) throws InvalidProtocolBufferException {
+    public static void main(String[] args) throws InvalidProtocolBufferException, JsonProcessingException {
         demo.pojo.User pojo = new demo.pojo.User(123, "demo");
         demo.proto.User proto = UserMapper.INSTANCE.map(pojo);
 
@@ -17,5 +28,13 @@ public class UserMapperMain {
         String json = "{\"id\":123,\"name\":\"demo\"}";
 
         System.out.println(JsonFormat.printer().omittingInsignificantWhitespace().print(proto));
+        System.out.println(JsonFormat.printer().omittingInsignificantWhitespace().print(
+                DemoCamel.newBuilder().setForgotAboutUnderscore("demo")));
+
+        List<User> users = asList(User.newBuilder().build());
+        Users wrapper = Users.newBuilder().addAllUsers(users).build();
+        wrapper.toByteArray();
+        System.out.println(JsonFormat.printer().omittingInsignificantWhitespace().print(wrapper));
+        System.out.println(new ObjectMapper().registerModule(new ProtobufModule()).writeValueAsString(users));
     }
 }
