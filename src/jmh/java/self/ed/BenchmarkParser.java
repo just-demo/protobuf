77d4a168a5cf069d009a0e01fdc90dcbc6ea9882
Anatolii Kryvda <anatolii.kryvda@automationanywhere.com>
2019-06-01 17:18:38 +0300

    More results

--- src/jmh/java/self/ed/BenchmarkParser.java
+++ src/jmh/java/self/ed/BenchmarkParser.java
@@ -8,16 +8,16 @@ import static org.apache.commons.lang3.StringUtils.leftPad;
 import static org.apache.commons.lang3.StringUtils.rightPad;
 
 public class BenchmarkParser {
-    static String benchmark = "TestDeserialization.bytesToProto_Protobuf       thrpt   25  749639.241 ± 15633.363  ops/s\n" +
-            "TestDeserialization.jsonToPojo_Jackson          thrpt   25  219149.582 ±  2501.725  ops/s\n" +
-            "TestDeserialization.jsonToPojo_Jackson_NoCache  thrpt   25   12011.588 ±   739.354  ops/s\n" +
-            "TestDeserialization.jsonToProto_Jackson         thrpt   25   51997.936 ±   513.459  ops/s\n" +
-            "TestDeserialization.jsonToProto_Protobuf        thrpt   25   40824.312 ±   595.746  ops/s\n" +
-            "TestSerialization.pojoToJson_Jackson            thrpt   25  528482.500 ±  7472.607  ops/s\n" +
-            "TestSerialization.pojoToJson_Jackson_NoCache    thrpt   25    6222.373 ±    54.745  ops/s\n" +
-            "TestSerialization.protoToBytes_Protobuf         thrpt   25  708904.513 ±  8327.902  ops/s\n" +
-            "TestSerialization.protoToJson_Jackson           thrpt   25   89316.938 ±  1165.873  ops/s\n" +
-            "TestSerialization.protoToJson_Protobuf          thrpt   25   56641.151 ±   739.160  ops/s";
+    static String benchmark = "TestDeserialization.bytesToProto_Protobuf          thrpt   25  632827.027 ±  8408.943  ops/s\n" +
+            "TestDeserialization.jsonToPojo_Jackson             thrpt   25  188470.423 ±  5015.404  ops/s\n" +
+            "TestDeserialization.jsonToPojo_Jackson_NoCache     thrpt   25   11285.449 ±   136.573  ops/s\n" +
+            "TestDeserialization.jsonToProto_Jackson            thrpt   25   51609.453 ±   618.650  ops/s\n" +
+            "TestDeserialization.jsonToProto_Protobuf           thrpt   25   41075.191 ±   502.794  ops/s\n" +
+            "TestSerialization.pojoToJson_Jackson               thrpt   25  521748.714 ±  7266.815  ops/s\n" +
+            "TestSerialization.pojoToJson_Jackson_NoCache       thrpt   25    6228.282 ±    92.065  ops/s\n" +
+            "TestSerialization.protoToBytes_Protobuf            thrpt   25  715850.351 ±  8349.113  ops/s\n" +
+            "TestSerialization.protoToJson_Jackson              thrpt   25   89574.020 ±  1071.878  ops/s\n" +
+            "TestSerialization.protoToJson_Protobuf             thrpt   25   55836.573 ±   693.589  ops/s";
 
     public static void main(String[] args) {
         Arrays.stream(benchmark.split("\n"))
